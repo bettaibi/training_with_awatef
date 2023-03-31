b67edb92cce3c69a94ee215f521086886b95738b
Nidhal <n.bettaibi@instadeep.com>
2023-03-31 16:45:53 +0100

    feat(web): project setup

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import About from "../About";
+
+export default function Home() {
+  return <div>Home</div>;
+}
+
+/***
+ *
+ *  git add . (add all changes to staging ) staging => files to be committed
+ *
+ *  git commit -m "adding new features"
+ *
+ *  git push (git push origin main)
+ */
+
+/**
+ *  Commit Conventions
+ *
+ *   feat(Web): "your message"
+ *
+ *   fix(web): "your message"
+ *
+ *   style(web): "your message"
+ *
+ *   refactor(web): "your message"
+ *
+ */
